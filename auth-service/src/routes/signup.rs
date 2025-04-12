dc9f01f0ed28986a9cb1c59f6084da9ef2c31054
reid <crt@rivvit.io>
2025-04-12 10:18:30 -0500

    [Sprint 1][Final]: fmt

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -13,14 +13,10 @@ pub struct SignupRequest {
 }
 
 pub async fn signup(Json(request): Json<SignupRequest>) -> impl IntoResponse {
-   println!("Received signup request: {:?}", request);
-
+    println!("Received signup request: {:?}", request);
     // Here you would typically handle the signup logic, such as saving the user to a database.
     // For this example, we will just return a 200 OK response.
-
     // Simulate some processing
     tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-
-    // Return a 200 OK response
    StatusCode::OK.into_response()
 }
