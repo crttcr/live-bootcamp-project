1b08d14ba1477b6adad11dfd27fdc2699e28f2b3
reid <crt@rivvit.io>
2025-04-25 16:22:36 -0500

    [Sprint 3][Task 5]: Working

--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -49,20 +49,24 @@ async fn should_return_401_if_banned_token() {
     let email    = Email::parse(email).unwrap();
     let token    = generate_auth_token(&email).unwrap();
     println!("token: {}", token);
-    
+
     // Now that we have a token, before we post it, let's add it to the
     // banded tokens set. After that, we should get a 401.
+    // We do this in a nested scope so our write lock is dropped before
+    // we post to the verify token endpoint.
     //
-    let mut tokens = app.banned_tokens.write().await;
-    tokens.add_token(token.clone()).await.unwrap();
-    println!("token added");
-    assert!(false);
-    
+    {
+        let mut tokens = app.banned_tokens.write().await;
+        tokens.add_token(token.clone()).await.unwrap();
+        println!("token added");
+    }
+
     // Now that we have added the token to the banned tokens set, let's
-    // post it.
+    // post to the verify token endpoint and ensure that we get a 401.
+    //
     let body     = json!({"token": token});
     println!("created body {}", body);
     let response = app.post_verify_token(&body).await;
     println!("Asserting result");
-    assert_eq!(response.status().as_u16(), 201);
+    assert_eq!(response.status().as_u16(), 401);
 }
